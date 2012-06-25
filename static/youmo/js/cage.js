615ec3d1157133c651a55b92de59ff870530ce82
cage <517020375@qq.com>
2012-06-25 22:30:25 +0800

    ..

--- static/youmo/js/cage.js
+++ static/youmo/js/cage.js
@@ -1,12 +1,221 @@
-var time = +new Date();
+var str="";
+str += "";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"230\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"147\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"437\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"217\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"187\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"197\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"127\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"157\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"267\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"177\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"177\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "				<li>";
+str += "					<a href=\"#\" class=\"picture\">";
+str += "						<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"140\" height=\"157\">";
+str += "					<\/a>";
+str += "					<div class=\"bar\">";
+str += "						<a href=\"#\">坏坏果丫头<\/a>";
+str += "						<span class=\"like\">11233<\/span>";
+str += "						<span class=\"reply\">123<\/span>";
+str += "					<\/div>";
+str += "					<div class=\"comment\">";
+str += "						<div class=\"media media3 clearfix\">";
+str += "							<div class=\"fleft pic\">";
+str += "								<a href=\"#\">";
+str += "									<img src=\"static\/youmo\/img\/img1.jpg\" alt=\"\" width=\"29\" height=\"29\">";
+str += "								<\/a>";
+str += "							<\/div>";
+str += "							<div class=\"txt\">";
+str += "								<p class=\"summary\">";
+str += "								<a href=\"#\" class=\"name\">鑫小七:<\/a>创作摄影师 2名潜力摄影助理 3名";
+str += "								<\/p>";
+str += "							<\/div>";
+str += "						<\/div>";
+str += "					<\/div>";
+str += "				<\/li>";
+str += "";
+
+
+//var time = +new Date();
 var youmo = {
 	waterfall : {
 		col : 4,
 		gap : 10,
 		cols : [],
+		maxH : 0,
 		init : function(obj){
 			this.water = typeof obj === "string" ? document.getElementById(obj) : obj;
-			this.maxH = 0;
 			var item = [];
 			var temp = this.water.childNodes;
 			var len = temp.length;
@@ -23,7 +232,6 @@ var youmo = {
 						this.cols[j]['height'] += oHtml.h;
 						this.maxH = Math.max(this.maxH,this.cols[j]["height"]);
 						if(i && (i + 1) % 4 === 0 && i < len - 3){
-							//console.log(this.maxH);
 							for(var k = 1; k <= 4; k++){
 								item[i + k].style.marginTop = this.cols[k - 1]["height"] - this.maxH + "px";
 							}
@@ -31,14 +239,45 @@ var youmo = {
 					}
 				}
 			}
-		
-			//console.log(item[0].offsetHeight + item[4].offsetHeight + 20);
-			//console.log(this);
 		},
-		load : function(){
-				   
+		load : function(data){
+			var oUl = document.createElement("ul");
+			oUl.innerHTML = data;
+			this.water.appendChild(oUl);
+			var item = [];
+			var temp = oUl.childNodes;
+			var len = temp.length;
+			while(len--){
+				temp[len].nodeType === 1 && item.unshift(temp[len]);
+			}
+			for(var i = 0, len = item.length; i < len; i++){
+				var oHtml = item[i];
+				oHtml.h = oHtml.offsetHeight + this.gap;
+				for(var j = 0; j < this.col; j++){
+					if(i % this.col === j){
+						if(i % 4 === 0){
+							for(var k = 0; k < 4; k++){
+								item[i + k].style.marginTop = this.cols[k]["height"] - this.maxH + "px";
+							}
+						}
+						!this.cols[j] && (this.cols[j] = {"items":[],"height":0});
+						this.cols[j]["items"].push(item[i]);
+						this.cols[j]['height'] += oHtml.h;
+						this.maxH = Math.max(this.maxH,this.cols[j]["height"]);
+					}
+				}
+			}
+			oUl.outerHTML = oUl.innerHTML;
 		}		
 	}
 };
 youmo.waterfall.init("waterfall");
-console.log(+new Date() - time);
+/*
+ *window.onscroll = function(){
+ *    var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+ *    document.title = document.body.clientHeight - document.documentElement.clientHeight + " ? " + scrolltop;
+ *    if(document.body.clientHeight - document.documentElement.clientHeight - scrolltop < 10){
+ *        youmo.waterfall.load(str);
+ *    }
+ *}
+ */
